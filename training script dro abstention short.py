e7834c9dff8c5fa7234bc57dc995ba1f5cb8ff8c
Surya Narayanan Hari <surya21@stanford.edu>
2022-04-18 15:53:38 -0400

    renamed files, cleaned up code

--- training script dro abstention short.py
+++ training script dro abstention short.py
@@ -6,30 +6,30 @@ if filter_tumor == 'True':
     dataset = dataset[dataset['prob_tumor'] > 0.5]
 
 #Assign labels
-kirk_whole_g24.loc[kirk_whole_g24['grade'] == 'G2', 'label'] = 0
-kirk_whole_g24.loc[kirk_whole_g24['grade'] == 'G4', 'label'] = 1
-kirk_whole_g24['label'] = torch.from_numpy(kirk_whole_g24['label'].values.astype(float)).type(torch.LongTensor) 
+dataset.loc[dataset['grade'] == 'G2', 'label'] = 0
+dataset.loc[dataset['grade'] == 'G4', 'label'] = 1
+dataset['label'] = torch.from_numpy(dataset['label'].values.astype(float)).type(torch.LongTensor) 
 
-kirk_whole_g24_sample = preprocess_df(kirk_whole_g24, num_hosps = num_unhealthy_hosps,
+dataset_sample = preprocess_df(dataset, num_hosps = num_unhealthy_hosps,
                                      num_tiles = num_tiles, 
                                      min_num_slides_p_hosp = min_num_slides_p_hosp, 
                                      min_num_tiles_p_slide = min_num_tiles_p_slide, 
                                      replace = replace
                                      )
 
-all_hosps = kirk_whole_g24['source_id'].unique()
+all_hosps = dataset['source_id'].unique()
 
 train_paths, val_paths = create_tp_vp(lp, GROUP_COL, train_size, random_state, label = 'healthy', num_classes = 2, replace = False)
 
 #Keeping track of the sources
 train_sources = sorted(list(set(train_paths['source_id'])))
 val_sources = sorted(list(set(val_paths['source_id'])))
-uniq_srcs = list(set(kirk_whole_g24['source_id']))
+uniq_srcs = list(set(dataset['source_id']))
 
 
 # train_paths = train_paths.sample(n = 1000, random_state = random_state)
 val_hosps = val_paths['source_id'].unique()
-val_set_whole = kirk_whole_g24[kirk_whole_g24['source_id'].isin(val_hosps)]
+val_set_whole = dataset[dataset['source_id'].isin(val_hosps)]
 
 val_paths = val_paths.sample(n = min(len(val_paths), config.num_val), random_state = random_state)
 val_set_sample = preprocess_df(val_set_whole, num_hosps = 'all', num_tiles = 200, min_num_slides_p_hosp = 0, min_num_tiles_p_slide = 200, replace = False)
@@ -89,12 +89,9 @@ trainer = pl.Trainer(gpus=1,
                     track_grad_norm =0,
                     callbacks=callbacks, 
                     logger=logger,  
-                    # limit_train_batches=50,
                     val_check_interval=config.val_check_interval,
-                    # limit_val_batches=10,
                     accumulate_grad_batches=config.accumulate_grad_batches,
                     log_every_n_steps=1,
-                    # min_steps=100, 
                     max_steps=config.max_steps,
                     fast_dev_run=0,
                     deterministic=True,
@@ -102,7 +99,3 @@ trainer = pl.Trainer(gpus=1,
 
 #Fit the trainer and start training
 trainer.fit(model, train_dataloader, val_dataloader)    
-
-torch.save(model.state_dict(), f'/home/jupyter/LUAD/Lung/weights/{EXP_VERSION}')
-
-model = nn.DataParallel(model).to('cuda')
